8c8ac2231198def7385537506b7db778948a7d0a
Exstreme_Noob <116436587+Exstreme-Noob@users.noreply.github.com>
2025-10-29 09:59:10 +0100

    refactor blat3 aufg 1

--- Prog_1/Blatt_3/Aufgabe_1/src/Main.java
+++ Prog_1/Blatt_3/Aufgabe_1/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("give me an interger you fleshbag!");
-int i = input.nextInt();
+        int i = input.nextInt();
         System.out.println("Zhe quersumme is "+Quersumme.quersumme(i));
     }
 }
--- Prog_1/Blatt_3/Aufgabe_1/src/Quersumme.java
+++ Prog_1/Blatt_3/Aufgabe_1/src/Quersumme.java
@@ -1,9 +1,9 @@
 public class Quersumme {
     public static int quersumme(int x) {
     int sum=0;
-    while (x != 0) {//wiederholt biss alle 1nerstellen addiert wurden
-        sum+=x%10;//modulo von 10 ist die 1-ner stelle
-        x=(x-(x%10))/10;//x ohne einzerstellelen wird durch 10 geteilt
+    while (x != 0) {         //wiederholt biss alle 1-ner stellen addiert wurden
+        sum+=x%10;          //modulo von 10 ist die 1-ner stelle
+        x=(x-(x%10))/10;    //x ohne einzerstellelen wird durch 10 geteilt
     }
     return sum;
     }
