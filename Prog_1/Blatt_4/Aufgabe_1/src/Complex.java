2ce9b98b4479e195599b61e498622765747b6b31
Eluminator05 <eluminator04@gmail.com>
2025-11-12 17:30:14 +0100

    Aufgabe_1 unfinished
    A1 b) fehlt der i² = -1 Testfall

---
+++ Prog_1/Blatt_4/Aufgabe_1/src/Complex.java
@@ -0,0 +1,91 @@
+// class to create complex numbers
+public class Complex {
+    double real;
+    double imaginary;
+
+    public Complex(double real, double imaginary){
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public Complex() {}
+
+    // adds two complex numbers
+    public Complex add(Complex other){
+        double realResult = this.real + other.real;
+        double imaginaryResult = this.imaginary + other.imaginary;
+        return new Complex(realResult, imaginaryResult);
+    }
+
+    // subtracts two complex numbers
+    public Complex sub(Complex other){
+        double realResult = this.real - other.real;
+        double imaginaryResult = this.imaginary - other.imaginary;
+        return new Complex(realResult, imaginaryResult);
+    }
+
+    // multiples two complex numbers
+    public Complex mul(Complex other){
+        double realResult = this.real*other.real - this.imaginary*other.imaginary;
+        double imaginaryResult = this.real * other.imaginary + this.imaginary * other.real;
+        return new Complex(realResult, imaginaryResult);
+    }
+
+    // divides called complex number by another complex number
+    public Complex div(Complex other){
+        double realNumerator = this.real*other.real + this.imaginary*other.imaginary;
+        double realDenominator = other.real * other.real + other.imaginary * other.imaginary;
+        double realResult = realNumerator / realDenominator;
+
+        double imaginaryNumerator = this.imaginary * other.real - this.real * other.imaginary;
+        double imaginaryDenominator = other.real * other.real + other.imaginary * other.imaginary;
+        double imaginaryResult = imaginaryNumerator / imaginaryDenominator;
+
+        return new Complex(realResult, imaginaryResult);
+    }
+
+    // returns a string representation of a complex number
+    public String toString(){
+        String name = "";
+        name += String.valueOf(this.real);
+        if (this.imaginary == 0.0) return name; // if imaginary == 0.0, don't even bother adding it
+        if(this.imaginary >= 0.0) name += "+";
+        name += String.valueOf(this.imaginary) + "i";
+        return name;
+    }
+
+    // checks if two complex numbers are equal
+    public boolean equals(Complex other){
+        if (Math.abs(this.real - other.real) >= 1e-12) return false;
+        if (Math.abs(this.imaginary - other.imaginary) >= 1e-12) return false;
+        return true;
+    }
+
+    // prints values of a complex
+    void print(){
+        System.out.println("Real number = " + this.real);
+        System.out.println("Imaginary number = " + this.imaginary);
+        System.out.println("Resulting complex number: " + this.toString() + "\n");
+    }
+
+    // compares given complex to complex expectedValue
+    void comparison(Complex expectedComplex) {
+        if (this.equals(expectedComplex)) {
+            System.out.println("result is (" + this + "). Ok\n");
+            return;
+        }
+        System.out.println("FAILED: expected (" + expectedComplex + "), got (" + this + ")\n");
+    }
+
+    /*
+    // compares given complex to complex expectedValue
+    void comparison(Complex expectedValue){
+        System.out.println("Calculated real number = " + this.real);
+        System.out.println("Calculated imaginary number = " + this.imaginary);
+        System.out.println("Expected real number = " + expectedValue.real);
+        System.out.println("Expected imaginary number = " + expectedValue.imaginary);
+        System.out.println("Calculated complex number equals expected complex number: " + this.equals(expectedValue) + "\n");
+    }
+
+     */
+}
---
+++ Prog_1/Blatt_4/Aufgabe_1/src/ComplexTest.java
@@ -0,0 +1,43 @@
+public class ComplexTest {
+    public static void main(String[] args) {
+        Complex testComplex = new Complex(3.0, -4.0);
+        Complex otherComplex = new Complex (-6.0, 8.0);
+        Complex emptyComplex = new Complex();
+        Complex expectedComplex;            // Variable for the expected result of a calculation, refer to the .pdf file containing the results
+        Complex result;                     // Variable for the programs result of a calculation
+
+        /*
+
+        // view all complex numbers
+        System.out.println("testComplex:");
+        testComplex.print();
+        System.out.println("otherComplex:");
+        otherComplex.print();
+        System.out.println("emptyComplex:");
+        emptyComplex.print();
+
+         */
+
+        System.out.print("Addition of (" + testComplex + ") and (" + otherComplex + "): ");     // Erkenntnis: wendet die toString()-Methode automatisch an
+        result = testComplex.add(otherComplex);
+        expectedComplex = new Complex(-3.0, 4.0);
+        result.comparison(expectedComplex);
+
+        System.out.print("Subtraction of (" + testComplex + ") and (" + otherComplex + "): ");
+        result = testComplex.sub(otherComplex);
+        expectedComplex = new Complex(9.0, -12.0);
+        result.comparison(expectedComplex);
+
+        System.out.print("Multiplication of (" + testComplex + ") and (" + otherComplex + "): ");
+        result = testComplex.mul(otherComplex);
+        expectedComplex = new Complex(14.0, 48.0);
+        result.comparison(expectedComplex);
+
+        System.out.print("Division of (" + testComplex + ") by (" + otherComplex + "): ");
+        result = testComplex.div(otherComplex);
+        expectedComplex = new Complex(-0.5, 0.0);
+        result.comparison(expectedComplex);
+
+    }
+
+}
