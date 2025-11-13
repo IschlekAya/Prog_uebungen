b93bceb7c98a9d17f71e85251add573b6e734714
Exstreme_Noob <116436587+Exstreme-Noob@users.noreply.github.com>
2025-11-13 12:15:01 +0100

    Blat_4/Aufgabe_2 b),c)

---
+++ Prog_1/Blatt_4/Aufgabe_2/src/Field.java
@@ -0,0 +1,39 @@
+public class Field {
+    private char[] fieldTypes = {'X', 'O', ' '};
+    private char[][] field = new char[3][3];
+
+    public Field() {
+        newField();
+    }
+
+    private void newField() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = fieldTypes[2];
+            }
+        }
+    }
+
+    public char[][] getField() {
+        return field;
+    }
+
+    public void makeMove(boolean player, int x, int y) {
+        if (player) {//true ist X
+            field[x][y] = fieldTypes[0];
+        } else {//false ist y
+            field[x][y] = fieldTypes[1];
+        }
+    }
+
+    public void printField() {
+        System.out.printf("%c|%c|%c\n" +
+                        "-+-+-\n" +
+                        "%c|%c|%c\n" +
+                        "-+-+-\n" +
+                        "%c|%c|%c\n",
+                field[0][0], field[1][0], field[2][0],
+                field[0][1], field[1][1], field[2][1],
+                field[0][2], field[1][2], field[2][2]);
+    }
+}
---
+++ Prog_1/Blatt_4/Aufgabe_2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+    Field feld = new Field();
+    feld.makeMove(true,1,1);
+    feld.makeMove(false,1,0);
+    feld.printField();
+    }
+}
