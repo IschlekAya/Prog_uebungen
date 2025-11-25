f7c7bfc9c48f558ddeb72e9f389e91c0a6025232
Exstreme_Noob <116436587+Exstreme-Noob@users.noreply.github.com>
2025-11-25 07:58:23 +0100

    rm redundant

--- Prog_1/Blatt_4/Aufgabe_2/src/Field.java
+++ Prog_1/Blatt_4/Aufgabe_2/src/Field.java
@@ -1,41 +1,14 @@
-// mit getField() zugrif auf das ganze feld
-// mit makeMove() züge spielen
-// mit resetField() feld auf leer zuruck zu machen
-// mit orintField() aktueles feld in conole printen (nur zur test zweken)
-public class Field {
-    private char[] fieldTypes = {'X', 'O', '#'};
-    private char[][] field = new char[3][3];
-    public Field() {
-        newField();
-    }
+// Properties a Field on the Board can have
+public enum Field {
+    CROSS,
+    CIRLCE,
+    EMPTY;
 
-    private void newField() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                field[i][j] = fieldTypes[2];
-            }
-        }
-    }
-
-    public char[][] getField() {
-        return field;
-    }
-    public void resetField(){
-        newField();
-    }
-    public void makeMove(boolean player, int x, int y) {
-        if (player) {//true ist X
-            field[x][y] = fieldTypes[0];
-        } else {//false ist y
-            field[x][y] = fieldTypes[1];
-        }
-    }
-    public void printField() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(field[i][j]);
-            }
-            System.out.print("\n");
+    public static Field toField(Player p){
+        switch (p){
+            case CROSS: return CROSS;
+            case CIRCLE: return CIRLCE;
+            default: return null;
         }
     }
 }
--- Prog_1/Blatt_4/Aufgabe_2/src/Main.java
+++ Prog_1/Blatt_4/Aufgabe_2/src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        //test code ✂
-    Field feld = new Field();
-    feld.makeMove(true,1,1);
-    feld.makeMove(false,1,0);
-    feld.printField();
+        Game.run();
     }
 }
