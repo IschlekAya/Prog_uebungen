37d7884b30630da74510b858370ff9521e722fa5
Eluminator05 <eluminator04@gmail.com>
2025-12-09 08:03:53 +0100

    cleanup
    renamed "radius" to "diameter"

--- Prog_1/Blatt_6/Aufgabe_1/src/Wheels.java
+++ Prog_1/Blatt_6/Aufgabe_1/src/Wheels.java
@@ -1,18 +1,15 @@
 public class Wheels {
-    private float radius;
+    private float diameter;
     private float width;
     private String brand;
 
     public Wheels(float radius0, float width0, String brand0){
-        radius = radius0;
+        diameter = radius0;
         width = width0;
         brand = brand0;
     }
 
-    @Override
-    public String toString(){return "Rad mit Radius " + getRadius() + ", dem Durchmesser " + getWidth() + " und der Marke " + getBrand();}
-
     public String getBrand(){return brand;}
-    public float getRadius(){return radius;}
+    public float getDiameter(){return diameter;}
     public float getWidth(){return width;}
 }
