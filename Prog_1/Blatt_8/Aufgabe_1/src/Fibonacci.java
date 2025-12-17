869704cf9c87500869bf18c74028ab2bd754444d
Eluminator05 <eluminator04@gmail.com>
2025-12-17 15:50:15 +0100

    finished
    it was that easy

---
+++ Prog_1/Blatt_8/Aufgabe_1/src/Fibonacci.java
@@ -0,0 +1,51 @@
+/**
+ * Reads an Integer i from the console and calculates the Fibonacci-number after i Iterations{@literal .}
+ * Prints the solution to the console{@literal .}
+ * Note: Only accurate up to and including the 48th Fibonacci-number
+ */
+public class Fibonacci {
+    /**
+     * Calculates the n-th Fibonacci-number using a for-loop
+     * @param n Amount of iterations
+     * @return Fibonacci-number after n iterations
+     */
+    public static int fibonacciIter(int n){
+        int a = 1;
+        int b = 0;
+        int c = 0;
+        for (int i = 0; i < n; i++){
+            c = a+b;
+//            System.out.printf("%d + %d = %d\n", a, b, c);
+            a = b;
+            b = c;
+//            System.out.printf("new a = %d, new b = %d\n", a, b);
+        }
+        return c;
+    }
+
+    /**
+     * Calculates the n-th Fibonacci-number using a recursive function
+     * @param n Amount of iterations
+     * @return Fibonacci-number after n iterations
+     */
+    public static int fibonacciRec(int n){
+        if (n > 0) return fibonacciRec(n, 1, 0);
+        return 0;
+    }
+
+    /**
+     * Coming from a fibonacci number, calculates the Fibonacci-number following after n-more iterations using a recursive function
+     * @param n Amount of iterations to follow
+     * @param a one part of the previous Fibonacci-number
+     * @param b other part of the previous Fibonacci-number
+     * @return Fibonacci-number after n more iterations
+     */
+    private static int fibonacciRec(int n, int a, int b){
+        if (n == 1) return a+b;
+        return  fibonacciRec((n-1), b, a+b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacciIter(Integer.parseInt(args[0])));
+    }
+}
---
+++ Prog_1/Blatt_8/Aufgabe_1/src/FibonacciBigInteger.java
@@ -0,0 +1,51 @@
+import java.math.BigInteger;
+/**
+ * Reads an Integer i from the console and calculates the Fibonacci-number after i Iterations{@literal .}
+ * Prints the solution to the console{@literal .}
+ */
+public class FibonacciBigInteger {
+    /**
+     * Calculates the n-th Fibonacci-number using a for-loop
+     * @param n Amount of iterations
+     * @return Fibonacci-number after n iterations as Integer
+     */
+    public static BigInteger fibonacciIter(int n){
+        BigInteger a = BigInteger.valueOf(1);
+        BigInteger b = BigInteger.valueOf(0);
+        BigInteger c = BigInteger.valueOf(0);
+        for (int i = 0; i < n; i++){
+            c = a.add(b);
+//            System.out.printf("%d + %d = %d\n", a, b, c);
+            a = b;
+            b = c;
+//            System.out.printf("new a = %d, new b = %d\n", a, b);
+        }
+        return c;
+    }
+
+    /**
+     * Calculates the n-th Fibonacci-number using a recursive function
+     * @param n Amount of iterations
+     * @return Fibonacci-number after n iterations as BigInteger
+     */
+    public static BigInteger fibonacciRec(int n){
+        if (n > 0) return fibonacciRec(n, BigInteger.valueOf(1), BigInteger.valueOf(0));
+        return BigInteger.valueOf(0);
+    }
+
+    /**
+     * Coming from a fibonacci number, calculates the Fibonacci-number following after n-more iterations using a recursive function
+     * @param n Amount of iterations to follow
+     * @param a one part of the previous Fibonacci-number
+     * @param b other part of the previous Fibonacci-number
+     * @return Fibonacci-number after n more iterations as BigInteger
+     */
+    private static BigInteger fibonacciRec(int n, BigInteger a, BigInteger b){
+        if (n == 1) return a.add(b);
+        return  fibonacciRec((n-1), b, a.add(b));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacciIter(Integer.parseInt(args[0])));
+    }
+}
